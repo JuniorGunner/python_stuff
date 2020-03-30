6a8b467c46c600ba221349552d90b495c673e70c
C. S. F. Junior <csf.junior90@gmail.com>
2020-03-30 17:17:33 -0300

    tab bug fixed, removed prints,

--- spider_generator/spider_gen.py
+++ spider_generator/spider_gen.py
@@ -4,32 +4,30 @@ import unidecode
 
 def read_excel():
     df = pd.read_excel("Path/To/city_list.xlsx")
-    cities = list(map(str.strip, df["CITY"].tolist())) # Creates a city list from DataFrame
+    cities = list(map(str.strip, df["CITY"].tolist()))
 
     return cities
 
 def generator(cities):
     for city in cities:
         
-        unaccent = unidecode.unidecode(city) # Remove word accent
+        unaccent = "vivareal_" + unidecode.unidecode(city) # Remove word accent
 
-        spider_name = unaccent.lower().replace(" ", "_") # spider
+        spider_name = unaccent.lower().replace(" ", "_") # Spider name pattern
 
-        link_apto = """ start_urls = [
-                    \t\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=APARTMENT&addressCity={}&size=100&from=000\",\n""".format(city)
-        link_home = "\t\t\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=HOME&addressCity={}&size=100&from=000\",\n".format(city)
-        link_land = "\t\t\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=RESIDENTIAL_ALLOTMENT_LAND&addressCity={}&size=100&from=000\",\n".format(city)
-        link_2story = "\t\t\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=TWO_STORY_HOUSE&addressCity={}&size=100&from=000\"]\n".format(city)
+        link_apto = """\n    start_urls = [ \"https://glue-api.vivareal.com/v1/listings?&filterUnitType=APARTMENT&addressCity={}&size=100&from=000\",\n""".format(city)
+        link_home = "\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=HOME&addressCity={}&size=100&from=000\",\n".format(city)
+        link_land = "\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=RESIDENTIAL_ALLOTMENT_LAND&addressCity={}&size=100&from=000\",\n".format(city)
+        link_2story = "\"https://glue-api.vivareal.com/v1/listings?&filterUnitType=TWO_STORY_HOUSE&addressCity={}&size=100&from=000\"]\n".format(city)
 
-        base_file = open("Scrapy/Project/Spiders//vivareal_my_city.py", "r")
+        base_file = open("Scrapy/Project/spiders/vivareal_city.py", "r")
         code_1 = base_file.read(1003)
         unused = base_file.read(709)
         code_2 = base_file.read(1938)
-        # print("\nUnused:\n\n", unused)
-        # print("\nCode 2:\n\n", code_2)
 
-        new_spider = open("Scrapy/Project/Spiders/vivareal_{}.py".format(spider_name), "a")
+        new_spider = open("Scrapy/Project/spiders/{}.py".format(spider_name), "a")
         new_spider.write(code_1)
+        new_spider.write(" \'" + spider_name + "\'")
         new_spider.write(link_apto)
         new_spider.write(link_home)
         new_spider.write(link_land)
@@ -41,5 +39,5 @@ def generator(cities):
 
 if __name__ == "__main__":
     cities = read_excel()
-    # print(cities)
+
     generator(cities)
